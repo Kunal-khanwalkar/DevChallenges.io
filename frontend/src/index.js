9381b05a2aa8a31615ea90bf708b77510e472dbb
Kunal Khanwalkar <kunalkhanwalkar@gmail.com>
2022-02-13 18:05:03 +0530

    Create React App

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const project = process.env.PROJECT_NAME;       // myproject
+
+ReactDOM.render(
+  <React.StrictMode>
+    <h1>Test</h1>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
